4765b1b0ca17a30121eae072166cea6a335c7797
Béres Olivér◘ <beresoli07@gmail.com>
2025-09-10 12:54:52 +0200

    játék kezdete

--- ElSo.py
+++ ElSo.py
@@ -5,3 +5,9 @@ for i in range(100):
     hlista.append(szam)
 #Ellenőrzés
 print(hlista)
+
+#Egyszám játék
+jatek_szam=0
+nem_talaltDB=0
+
+kitalalando_szam=hlista[random.randint(0,len(hlista))] #0-tól kezdi egyébként a randomgenerálást
