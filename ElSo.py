4c2a49a391cb813acf044e7ef8e70c338d4becaa
Béres Olivér◘ <beresoli07@gmail.com>
2025-09-10 13:29:36 +0200

    műkköd első vers

--- ElSo.py
+++ ElSo.py
@@ -11,3 +11,15 @@ jatek_szam=0
 nem_talaltDB=0
 
 kitalalando_szam=hlista[random.randint(0,len(hlista))] #0-tól kezdi egyébként a randomgenerálást
+
+tipp_szám=int(input("Tippelj egy egész számot: "))
+
+while tipp_szám!=kitalalando_szam:
+    print("Nem talált!")
+    tipp_szám=int(input("Tippelj újat: "))
+    
+print("Eltaláltad!")
+
+akarsze=input("Akarsz még játszani (I/N): ")
+
+if ()
